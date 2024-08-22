b01ce093f6601707a0c3d1574808719745a85678
gaasuu2046 <fh6edpizf@gmail.com>
2024-08-22 17:49:24 +0000

    review fix

--- app/app/api/review/route.ts
+++ app/app/api/review/route.ts
@@ -12,9 +12,9 @@ export async function GET() {
 
 export async function POST(request: Request) {
   const body = await request.json()
-  const { name, description, category, brand } = body
-  const gear = await prisma.gear.create({
-    data: { name, description, category, brand },
+  const { rating, comment, gearId } = body
+  const gear = await prisma.review.create({
+    data: { rating, comment, gearId },
   })
   return NextResponse.json(gear, { status: 201 })
 }
