c469dfe2d02ee0e2393d40c93366b6f322caf812
gaasuu2046 <fh6edpizf@gmail.com>
2024-08-22 18:34:17 +0000

    up

--- app/app/gear/page.tsx
+++ app/app/gear/page.tsx
@@ -1,14 +1,23 @@
 // app/gear/page.tsx
 import Link from 'next/link'
+import { notFound } from 'next/navigation'
 
-const gears = [
-  { id: 1, name: 'テント', brand: 'アウトドアブランドA', rating: 4.5 },
-  { id: 2, name: '寝袋', brand: 'アウトドアブランドB', rating: 4.2 },
-  { id: 3, name: 'バックパック', brand: 'アウトドアブランドC', rating: 4.8 },
-  { id: 4, name: 'トレッキングポール', brand: 'アウトドアブランドD', rating: 4.3 },
-  { id: 5, name: 'UL靴', brand: 'アウトドアブランドE', rating: 4.6 },
-  { id: 6, name: 'ヘッドランプ', brand: 'アウトドアブランドF', rating: 4.1 },
-]
+import prisma from '@/lib/prisma'
+
+async function getGearList() {
+  const gearList = await prisma.gear.findMany()
+  if (!gearList) notFound()
+  return gearList
+}
+// const gears = [
+//   { id: 1, name: 'テント', brand: 'アウトドアブランドA', rating: 4.5 },
+//   { id: 2, name: '寝袋', brand: 'アウトドアブランドB', rating: 4.2 },
+//   { id: 3, name: 'バックパック', brand: 'アウトドアブランドC', rating: 4.8 },
+//   { id: 4, name: 'トレッキングポール', brand: 'アウトドアブランドD', rating: 4.3 },
+//   { id: 5, name: 'UL靴', brand: 'アウトドアブランドE', rating: 4.6 },
+//   { id: 6, name: 'ヘッドランプ', brand: 'アウトドアブランドF', rating: 4.1 },
+// 
+const gears = await getGearList();
 
 export default function GearList() {
   return (
@@ -18,8 +27,9 @@ export default function GearList() {
         {gears.map((gear) => (
           <div key={gear.id} className="border rounded-lg p-4 shadow-md">
             <h2 className="text-xl font-semibold mb-2">{gear.name}</h2>
-            <p className="text-gray-600 mb-2">{gear.brand}</p>
-            <p className="text-yellow-500 mb-2">{'★'.repeat(Math.round(gear.rating))}{'☆'.repeat(5 - Math.round(gear.rating))}</p>
+            <img src={gear.img} alt={gear.name} className="h-48 w-full object-cover object-center mb-2" />
+            <p className="text-gray-600 mb-2">{gear.category}</p>
+            {/* <p className="text-yellow-500 mb-2">{'★'.repeat(Math.round(gear.rating))}{'☆'.repeat(5 - Math.round(gear.rating))}</p> */}
             <Link href={`/gear/${gear.id}`} className="text-blue-500 hover:underline">
               詳細を見る
             </Link>
