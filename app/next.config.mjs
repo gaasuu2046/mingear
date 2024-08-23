d6d4c95e0f0cea623a659a3c62a1767558622037
gaasuu2046 <fh6edpizf@gmail.com>
2024-08-23 15:07:47 +0000

    relative path fix and shape lib add

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const nextConfig = {
   output: 'standalone',
   images: {
@@ -9,6 +14,10 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  }),
 }
 
 export default nextConfig
